c3fe8f789f3af7de7135865ca02c0809d00c8517
ForteMare <30961251+ForteMare@users.noreply.github.com>
2020-05-22 22:01:36 +0200

    different js approach required

--- scripts/realestate.js
+++ scripts/realestate.js
@@ -1,8 +1,12 @@
+// NOT WORKING. DIFFERENT METHOD WILL BE APPLIED
+
 const estates = document.querySelectorAll("estate");
+const title = document.createElement("h3");
+title.innerHTML = "TEST CREATION";
 
 window.addEventListener("load", function () {
   document.body.style.backgroundColor = "#81188c";
   for (let i = 0; i < estates.length; i++) {
-    estates[i].innerHTML += "Testing";
+    estates[i].appendChild(title);
   }
 });
