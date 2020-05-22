90ba71cc1ca52f1676c4af57e8995313fc264898
ForteMare <30961251+ForteMare@users.noreply.github.com>
2020-05-22 21:13:55 +0200

    automatization of cards

--- scripts/realestate.js
+++ scripts/realestate.js
@@ -1,1 +1,8 @@
-var estates = document.getElementsByClassName("estate");
+const estates = document.querySelectorAll("estate");
+
+window.addEventListener("load", function () {
+  document.body.style.backgroundColor = "#81188c";
+  for (let i = 0; i < estates.length; i++) {
+    estates[i].innerHTML += "Testing";
+  }
+});
