b9aa1e619ae2b586978a9d21af356dd23c2a09d8
ForteMare <30961251+ForteMare@users.noreply.github.com>
2020-05-24 17:36:47 +0200

    added comments

--- scripts/realestate.js
+++ scripts/realestate.js
@@ -16,12 +16,16 @@ request.onload = function () {
 // Adding content to the webpage
 
 function fillData(jsonObj) {
+  // Showcase is the parent div under which estate classes are added
   const showcase = document.querySelector(".showcase");
+
+  // Get object elements from JSON
   const propertyName = jsonObj["name"];
   const propertyRegion = jsonObj["region"];
   const propertyPrice = jsonObj["price"];
   const propertyBedrooms = jsonObj["bedrooms"];
 
+  // Create as many cards as there are items in propertyName array
   for (let i = 0; i < propertyName.length; i++) {
     // Create div card and set class to estate
     const estate = document.createElement("div");
