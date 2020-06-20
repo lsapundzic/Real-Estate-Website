3df039e6cf73e0ab6d9092ba3427aeb338851064
ForteMare <30961251+ForteMare@users.noreply.github.com>
2020-06-20 19:41:21 +0200

    landing page switched to PHP

---
+++ elements/footer.php
@@ -0,0 +1,36 @@
+<footer>
+    <!-- Left Footer -->
+    <div class="ft-slice">
+        Contact us: <br />
+        <address>
+            <a href="mailto:sales@aera.si">sales@aera.si</a> <br />
+            Glavni trg 12 <br />
+            8000 Novo mesto <br />
+            Slovenia <br />
+        </address>
+        Or find us on social media: <br />
+        <a href="#">Facebook Page</a> |
+        <a href="#">Instagram Page</a> <br />
+    </div>
+
+    <!-- Center Footer -->
+    <div class="ft-slice ft-map">
+        <iframe class="gmap" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d11126.048487717624!2d15.1672567!3d45.8010011!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ssi!4v1590139467594!5m2!1sen!2ssi" width="470" height="230" frameborder="0" style="border: 0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+
+    <!-- Right Footer -->
+    <div class="ft-slice">
+        Or find us in our international offices around the world:
+
+        <!-- TODO: Not working yet -->
+        <ul>
+            <li>Barcelona</li>
+            <li>Madrid</li>
+            <li>NYC</li>
+            <li>Shanghai</li>
+        </ul>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ elements/head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="real estate lower carniola showcase" />
+    <meta name="company name" content="Aera Real Estate d.o.o." />
+    <title>Æera Real Estate Agency| Home</title>
+
+    <!-- Main CSS Style -->
+    <link rel="stylesheet" href="styles/style.css" />
+
+    <!-- CSS for Externals -->
+    <link rel="stylesheet" href="styles/external.css" />
+
+    <!-- Main JS Script -->
+    <script src="scripts/script.js" defer></script>
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;1,300;1,900&family=Oswald:wght@500;700&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav>
+        <a href="#">Home</a>
+        <a href="./pages/about.html">About</a>
+        <a href="./pages/realestate.html">Real Estate</a>
+        <a href="./pages/lifestyle.html">Lifestyle</a>
+        <a href="./pages/contact.html">Contact</a>
+    </nav>
