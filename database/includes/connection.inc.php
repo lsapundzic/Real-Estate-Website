627d525960837757d5b46e1e4b79e199f5a43833
ForteMare <30961251+ForteMare@users.noreply.github.com>
2020-07-01 20:56:50 +0200

    Real estate data is read from local SQL DB

--- database/includes/connection.inc.php
+++ database/includes/connection.inc.php
@@ -1,1 +1,13 @@
 <?php
+// Code for connecting to the database
+
+// Set up for XAMPP
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+
+// Depends on how the database is called in SQL
+$dbName = "real_estate";
+
+// Connection
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
--- pages/realestate.php
+++ pages/realestate.php
@@ -1,5 +1,6 @@
 <?php
-include "../components/header.estates.php"
+include "../components/header.estates.php";
+include_once "../database/includes/connection.inc.php";
 ?>
 
 <!-- Main Content -->
@@ -12,8 +13,33 @@ include "../components/header.estates.php"
             <div class="bttn" id="gorenjska">Gorenjska</div>
         </div>
     </aside>
-    <div class="showcase">
 
+    <!--    Main Page Content-->
+    <div class="showcase">
+        <?php
+        // Variable that holds SQL table
+        $sql = "SELECT * FROM properties;";
+
+        // Get a result of query $sql from $conn connection
+        $result = mysqli_query($conn, $sql);
+
+        // Checking if database actually returns something. Safety measure
+        $resultCheck = mysqli_num_rows($result);
+
+        // Second part of the safety measure
+        if ($resultCheck > 0) {
+
+            // Keep calling from DB as long as there is data
+            // Assign data from $result to an array $row
+            while ($row = mysqli_fetch_assoc($result)) {
+
+                // Filling out the fields
+                echo "<div class='estate'><h3>" . $row['name'] . "</h3><h4>" . $row['region'] . "</h4><p>&euro;"
+                    . $row['price'] . "</p><p>Bedrooms: " . $row['bedrooms'] . "</p></div>";
+//                echo $row['name'] . "<br>";
+            }
+        }
+        ?>
     </div>
 </main>
 
