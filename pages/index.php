b0fcdb5ff1f36f608b5ca2bc9b92c371f1f96981
ForteMare <30961251+ForteMare@users.noreply.github.com>
2020-07-02 23:04:54 +0200

    Final for tonight: pictures enabled, background tested, comments added

--- pages/index.php
+++ pages/index.php
@@ -5,36 +5,36 @@ include "../components/head_a.php";
 include "../components/head_b.php";
 ?>
 
-<!-- Header-Banner -->
-<header>
-    <h1>Æera Real Estate Agency</h1>
-    <h2>The Art of Architecture | Luxury Properties</h2>
-</header>
+    <!-- Header-Banner -->
+    <header>
+        <h1>Æera Real Estate Agency</h1>
+        <h2>The Art of Architecture | Luxury Properties</h2>
+    </header>
 
-<!-- Main Content -->
-<main>
-    <section>
-        <h3>Welcome</h3>
-        <article>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-                asperiores tenetur earum accusamus minus distinctio ut dolorum, iure
-                iusto illo facere autem tempora provident vero rerum. Saepe natus
-                est placeat modi inventore mollitia reprehenderit praesentium
-                cumque. Eos impedit consectetur saepe ad blanditiis repudiandae ab,
-                architecto harum ipsam quos voluptates eaque dolorum veniam totam!
-                Nisi libero quo corrupti omnis, neque sapiente, qui, sunt temporibus
-                nesciunt quisquam nulla atque est inventore totam voluptas maiores
-                quam autem ipsum odio? Debitis quam fugiat reprehenderit corporis at
-                necessitatibus aperiam dicta, eligendi aspernatur. Explicabo,
-                tenetur ullam? Molestias amet consectetur sequi labore eum
-                blanditiis dicta. Adipisci, culpa!
-            </p>
-        </article>
-    </section>
-</main>
+    <!-- Main Content -->
+    <main>
+        <section>
+            <h3>Welcome</h3>
+            <article>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
+                    asperiores tenetur earum accusamus minus distinctio ut dolorum, iure
+                    iusto illo facere autem tempora provident vero rerum. Saepe natus
+                    est placeat modi inventore mollitia reprehenderit praesentium
+                    cumque. Eos impedit consectetur saepe ad blanditiis repudiandae ab,
+                    architecto harum ipsam quos voluptates eaque dolorum veniam totam!
+                    Nisi libero quo corrupti omnis, neque sapiente, qui, sunt temporibus
+                    nesciunt quisquam nulla atque est inventore totam voluptas maiores
+                    quam autem ipsum odio? Debitis quam fugiat reprehenderit corporis at
+                    necessitatibus aperiam dicta, eligendi aspernatur. Explicabo,
+                    tenetur ullam? Molestias amet consectetur sequi labore eum
+                    blanditiis dicta. Adipisci, culpa!
+                </p>
+            </article>
+        </section>
+    </main>
 
-<!-- Footer -->
+    <!-- Footer -->
 
 <?php
 include "../components/footer.php"
