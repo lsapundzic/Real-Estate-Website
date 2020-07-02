f4746b04ade49bcbae884f818534851762ec9655
ForteMare <30961251+ForteMare@users.noreply.github.com>
2020-07-02 18:01:14 +0200

    Calling SQL elements into custom page
    Changed button ids

--- pages/estate.list.php
+++ pages/estate.list.php
@@ -1,6 +1,17 @@
 <?php
 include "../components/estate.header.php";
 include_once "../database/includes/connection.inc.php";
+
+// Getting data from SQL is here
+
+// Variable that holds SQL table
+$sql = "SELECT * FROM properties;";
+
+// Get a result of query $sql from $conn connection
+$result = mysqli_query($conn, $sql);
+
+// Checking if database actually returns something. Safety measure
+$resultCheck = mysqli_num_rows($result);
 ?>
 
 <!-- Main Content -->
@@ -9,13 +20,13 @@ include_once "../database/includes/connection.inc.php";
         <h4>Filter Content:</h4>
         <div class="buttonsBox">
             <div class="bttn" id="all">ALL</div>
-            <div class="bttn" id="gorenjska">Littoral</div>
-            <div class="bttn" id="gorenjska">Upper Carniola</div>
-            <div class="bttn" id="gorenjska">Inner Carniola</div>
-            <div class="bttn" id="gorenjska">Lower Carniola</div>
-            <div class="bttn" id="gorenjska">Carinthia</div>
-            <div class="bttn" id="gorenjska">Styria</div>
-            <div class="bttn" id="gorenjska">Prekmurje</div>
+            <div class="bttn" id="littoral">Littoral</div>
+            <div class="bttn" id="upper">Upper Carniola</div>
+            <div class="bttn" id="inner">Inner Carniola</div>
+            <div class="bttn" id="lower">Lower Carniola</div>
+            <div class="bttn" id="carinthia">Carinthia</div>
+            <div class="bttn" id="styria">Styria</div>
+            <div class="bttn" id="prekmurje">Prekmurje</div>
 
         </div>
     </aside>
@@ -23,14 +34,6 @@ include_once "../database/includes/connection.inc.php";
     <!--    Main Page Content-->
     <div class="showcase">
         <?php
-        // Variable that holds SQL table
-        $sql = "SELECT * FROM properties;";
-
-        // Get a result of query $sql from $conn connection
-        $result = mysqli_query($conn, $sql);
-
-        // Checking if database actually returns something. Safety measure
-        $resultCheck = mysqli_num_rows($result);
 
         // Second part of the safety measure
         if ($resultCheck > 0) {
--- pages/estate.single.php
+++ pages/estate.single.php
@@ -2,7 +2,29 @@
 include_once "../database/includes/connection.inc.php";
 include "../components/header.php";
 
-$db_id = $_GET['id'];
+// Get ID passed from estate.list.php
+$id_local = $_GET['id'];
+
+
+// Variable that holds SQL table row where ID matches GET
+$sql_local = "SELECT * FROM properties WHERE id='{$id_local}';";
+
+// Get a result of query $sql from $conn connection
+$result = mysqli_query($conn, $sql_local);
+
+// Checking if database actually returns something. Safety measure
+$resultCheck = mysqli_num_rows($result);
+
+// Second part of the safety measure
+if ($resultCheck > 0) {
+
+    // Keep calling from DB as long as there is data
+    // Assign data from $result to an array $row
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        $row_data = $row['name'];
+    }
+}
 
 ?>
 
@@ -10,7 +32,7 @@ $db_id = $_GET['id'];
     <header>
         <h1>YES! Your Reached It!</h1>
         <h2>The Art of Architecture | Luxury Properties</h2>
-        <h2><?php echo "The passed id of this page is: {$db_id}" ?></h2>
+        <h2><?php echo "The passed id of this page is: {$id_local}" ?></h2>
     </header>
 
     <!-- Main Content -->
@@ -19,6 +41,13 @@ $db_id = $_GET['id'];
             <h3>Welcome to Individual Real Estate Page</h3>
             <section>
                 <p>
+                    <?php
+
+                    echo "*DISPLAYED HERE*{$row_data}";
+                    ?>
+                    <br>
+                    <br>
+                    <br>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
                     asperiores tenetur earum accusamus minus distinctio ut dolorum, iure
                     iusto illo facere autem tempora provident vero rerum. Saepe natus
