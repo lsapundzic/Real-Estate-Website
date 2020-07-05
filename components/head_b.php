62f8b34d7f25eb2b0c7bc15c7792e101167a217e
ForteMare <30961251+ForteMare@users.noreply.github.com>
2020-07-05 19:05:48 +0200

    Mobile menu functional

--- components/head_b.php
+++ components/head_b.php
@@ -8,7 +8,7 @@
 
 <body>
 
-<!-- Navbar -->
+<!-- Navbar for desktop -->
 
 <nav>
     <ul>
@@ -29,3 +29,30 @@
         </li>
     </ul>
 </nav>
+
+<!-- Navbar for mobile -->
+<div class="mobile-nav">
+    <div class="dropdown-menu">
+        <div class="dropdown-button">Menu
+            <div class="dropdown-content">
+                <ul>
+                    <li>
+                        <a href="../pages/index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="../pages/about.php">About</a>
+                    </li>
+                    <li>
+                        <a href="../pages/estate.list.php">Real Estate</a>
+                    </li>
+                    <li>
+                        <a href="../pages/contact.php">Contact</a>
+                    </li>
+                    <li>
+                        <a href="../pages/login.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
