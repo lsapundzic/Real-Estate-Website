b9db8f9688c4ad67b0028e66ea4d6b0e474a9204
Luka <30961251+lsapundzic@users.noreply.github.com>
2022-01-12 22:39:01 +0100

    Formatted

--- scripts/script.js
+++ scripts/script.js
@@ -5,55 +5,52 @@
 // Select all cards on the screen
 const cards = document.querySelectorAll(".estate");
 
-$('#all').click(function () {
-    refreshList();
-})
+$("#all").click(function () {
+  refreshList();
+});
 
-$('#littoral').click(function () {
-    searchList("Littoral");
-})
+$("#littoral").click(function () {
+  searchList("Littoral");
+});
 
-$('#upper').click(function () {
-    searchList("Upper Carniola");
-})
+$("#upper").click(function () {
+  searchList("Upper Carniola");
+});
 
-$('#inner').click(function () {
-    searchList("Inner Carniola");
-})
+$("#inner").click(function () {
+  searchList("Inner Carniola");
+});
 
-$('#lower').click(function () {
-    searchList("Lower Carniola");
-})
+$("#lower").click(function () {
+  searchList("Lower Carniola");
+});
 
-$('#carinthia').click(function () {
-    searchList("Carinthia");
-})
+$("#carinthia").click(function () {
+  searchList("Carinthia");
+});
 
-$('#styria').click(function () {
-    searchList("Styria");
-})
+$("#styria").click(function () {
+  searchList("Styria");
+});
 
-$('#prekmurje').click(function () {
-    searchList("Prekmurje");
-})
-
-
-
+$("#prekmurje").click(function () {
+  searchList("Prekmurje");
+});
 
 function refreshList() {
-    for (let i = 0; i < cards.length; i++) {
-        $(cards[i]).show();
-    }
+  for (let i = 0; i < cards.length; i++) {
+    $(cards[i]).show();
+  }
 }
 
 function searchList(x) {
-    // Refreshes the list so that the searchList function can start anew
-    refreshList();
+  // Refreshes the list so that the searchList function can start anew
+  refreshList();
 
-    for (let i = 0; i < cards.length; i++) {
-        // .innerText is crucial here, makes the comparison possible
-        if (cards[i].querySelector("h4").innerText !== x) {
-            $(cards[i]).fadeOut(1500);
-        }
+  for (let i = 0; i < cards.length; i++) {
+    // .innerText is crucial here, makes the comparison possible
+    if (cards[i].querySelector("h4").innerText !== x) {
+      $(cards[i]).fadeOut(1500);
     }
+  }
 }
